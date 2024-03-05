be6c699b5a33176732266e228a63f71c9fa8e6ff
gsx <2280933632@qq.com>
2024-03-05 18:11:28 +0800

    4sum

--- hash/hash_basic.cpp
+++ hash/hash_basic.cpp
@@ -6,11 +6,125 @@
 #include <map>
 #include <unordered_map>
 #include <unordered_set>
+#include <algorithm>
 using namespace std;
 
 
-// leetcode 383
+// leetcode18 fourSum
+vector<vector<int>> fourSum(vector<int>& nums, int target) {
+    vector<vector<int>> result;
+    sort(nums.begin(), nums.end());
+    int size = nums.size();
+    // a + b + c + d，由外及里，每一层都要进行去重操作
+    //叠加两层for循环，再使用双指针求解
+    for (int i = 0; i < size; i++) {
+        if(nums[i] > target) return result;
+        if(i > 0 && nums[i] == nums[i-1]) continue;
+        for(int j = i + 1; j < size; j ++){
+            int sum12 = nums[i] + nums[j];
+            //双指针遍历
+            int left = j + 1;
+            int right = nums.size() - 1;
+            while(right>left){
+                //左右指针去重
+                int sum = nums[left] + nums[right] + sum12;
+                if(sum > 0){
+                    right --;
+                }else if(sum < 0){
+                    left ++;
+                }else{
+
+                }
+                right --;
+                left ++;
+            }
+        }
+    }
+    return result;
+}
+
+
+// leetcode15 2 pointers
+// 三个数字下标彼此不同
+// 相同的三元组只能出现一次，如{-1,-1,2}可能出现多次，但在答案中只能出现一次
+// 所以需要在求得一个解以后进行去重操作，排除和当前相同的
+vector<vector<int>> threeSum(vector<int>& nums) {
+    vector<vector<int>> result;
+    sort(nums.begin(), nums.end());
+    for(int i = 0; i < nums.size(); i++){
+        //初始判断条件
+        if(nums[i]>0) return result;
+        //对a去重，去重的目的在于去除重复的三元组合
+        //合理性在于，nums已排序，所以相同的元素都是相邻的，对于a而言
+        if(i >0 && nums[i] == nums[i-1]) {
+            continue;
+        }
+        //双指针遍历，对每个a可能出现的所有组合做一次遍历
+        int left = i + 1;
+        int right = nums.size()-1;
+        while(right > left){
+            if(nums[i]+nums[left]+nums[right] > 0){
+                right --;
+            } else if(nums[i]+nums[left]+nums[right] < 0){
+                left ++;
+            } else {
+                result.push_back({nums[i], nums[left], nums[right]});
+                //对right, left去重，跳出while时，left\right 都指向一连串重复值中的最后一个
+                while(right > left && nums[right] == nums[right-1]) right--;
+                while(right > left && nums[left] == nums[left+1]) left++;
+                right--;
+                left++;
+            }
+        }
+    }
+    return result;
+}
+
+// leetcode 15
+vector<vector<int>> threeSum1(vector<int>& nums) {
+    vector<vector<int>> ans;
+    unordered_map<int,vector<int>> sum12;
+    for(int i = 0; i < nums.size(); i++){
+        for(int j = i + 1; j < nums.size(); j++){
+            int sum = nums[i] + nums[j];
+            sum12[i].push_back(i);
+            sum12[i].push_back(j);
+        }
+    }
+    for(int i = 0; i < nums.size(); i++){
+        int complement = -nums[i];
+        // 找到符合条件的值，再判断是否有重叠项
+        auto it = sum12.find(complement);
+        if(it != sum12.end()){
+            vector<int> sub_ans = it->second;
+            // jesus, this is tough, let's try another method.
+            //auto it2 = find(sub_ans.begin(),sub_ans.end(), i);
+
+        }
+    }
+
+    return ans;
+}
+
+
+// leetcode 383-2
 bool canConstruct(string ransomNote, string magazine) {
+    int record[26] = {0};
+    if(magazine.size() < ransomNote.size()) return false;
+    for (char mag:magazine) {
+        record[mag-'a']++;
+    }
+    for (char ran:ransomNote) {
+        record[ran -'a']--;
+    }
+    for(int i = 0; i < 26; i ++){
+        if(record[i] < 0) return false;
+    }
+    return true;
+}
+
+// leetcode 383-1
+bool canConstruct2(string ransomNote, string magazine) {
     unordered_map<char, int> mag_cha;
     for(char m:magazine){
         mag_cha[m]++;
